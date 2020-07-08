90f1aa070a93ed5635c2611dfde19c1a1019ab4c
sixkery <sixkery@163.com>
2020-07-08 18:53:42 +0800

    导入依赖

--- code-docker/src/main/java/com/sixkery/CodeDockerApplication.java
+++ code-docker/src/main/java/com/sixkery/CodeDockerApplication.java
@@ -1,8 +1,10 @@
 package com.sixkery;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = "com.sixkery.dao")
 @SpringBootApplication
 public class CodeDockerApplication {
 
--- code-docker/src/main/java/com/sixkery/web/user/LoginFrom.java
+++ code-docker/src/main/java/com/sixkery/web/user/LoginFrom.java
@@ -2,7 +2,6 @@ package com.sixkery.web.user;
 
 import lombok.Data;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 /**
