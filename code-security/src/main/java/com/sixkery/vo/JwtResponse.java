1bb50b912630de69143d35d7ee1622a644d07351
sixkery <sixkery@163.com>
2020-07-17 13:30:28 +0800

    提交测试用例

--- code-security/src/main/java/com/sixkery/vo/JwtResponse.java
+++ code-security/src/main/java/com/sixkery/vo/JwtResponse.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * <p>
  * JWT 响应返回
@@ -26,4 +30,31 @@ public class JwtResponse {
     public JwtResponse(String token) {
         this.token = token;
     }
+
+
+    public static void main(String[] args) {
+                HashMapThread thread0 = new HashMapThread();
+                 HashMapThread thread1 = new HashMapThread();
+                 HashMapThread thread2 = new HashMapThread();
+                 HashMapThread thread3 = new HashMapThread();
+                 HashMapThread thread4 = new HashMapThread();
+                 thread0.start();
+                 thread1.start();
+                 thread2.start();
+                 thread3.start();
+                 thread4.start();
+    }
+
 }
+class HashMapThread extends Thread {
+private static AtomicInteger ai = new AtomicInteger();
+private static Map<Integer, Integer> map = new HashMap<>();
+
+        @Override
+public void run() {
+        while (ai.get() < 1000000) {
+                         map.put(ai.get(), ai.get());
+                         ai.incrementAndGet();
+                     }
+             }
+ }
